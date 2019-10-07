298ea3425fdad8cebefb6de0370b90c1a79b3dfb
Eric Gish <gisheri@gmail.com>
2019-10-07 08:35:17 -0600

    Add seeding option

--- index.js
+++ index.js
@@ -4,30 +4,46 @@ const data = {
     names: require('./names.json'),
     adjectives: require('./adjectives.json')
 }
-let seperator = '-'
 
-function setNames(names) {
-    data.names = names
-};
-
-function setAdjectives(adjectives) {
-    data.adjectives = adjectives
-};
-
-function setSeperator(new_seperator) {
-    seperator = new_seperator
-};
-
-function generate() {
-    const ran_a = Math.floor(Math.random() * data.names.length)
-    const ran_b = Math.floor(Math.random() * data.adjectives.length)
-    const ran_suffix = Math.floor(Math.random() * 100)
-    return `${data.adjectives[ran_b]}${seperator}${data.names[ran_a]}${ran_suffix}`
-};
-
-module.exports = {
-    setNames: setNames,
-    setAdjectives: setAdjectives,
-    setSeperator: setSeperator,
-    generate: generate
+const alphaToNumeric = (string) => {
+  let numeric = string.toLowerCase().split("").map(char=>char.charCodeAt(0) - 97 + 1).join("");
+  return parseInt(numeric);
 }
+
+const seededRandom = (seed) => {
+  if(!seed){
+    return Math.random();
+  }
+  if(isNaN(seed)){
+      seed = alphaToNumeric(seed);
+  }
+  let x = Math.sin(seed++) * 10000;
+  return x - Math.floor(x);
+}
+
+const generator = {
+    seed: null,
+    adjectives: data.adjectives,
+    names: data.names,
+    setAdjectives(adjectives) {
+        this.adjectives = adjectives
+    },
+    setNames(name){
+        this.names = names
+    },
+    setSeparator(separator) {
+        this.separator = separator   
+    },
+    setSeed(seed){
+        this.seed = seed;
+    },
+    generate(){
+        let random = seededRandom(this.seed)
+        const ran_a = Math.floor(random * data.names.length)
+        const ran_b = Math.floor(random * data.adjectives.length)
+        const ran_suffix = Math.floor(random * 100)
+        return `${data.adjectives[ran_b]}${seperator}${data.names[ran_a]}${ran_suffix}`
+    }
+}
+
+module.exports = generator;
